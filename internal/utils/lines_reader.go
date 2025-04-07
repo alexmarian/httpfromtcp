e9cbc286b40a5701ee5fafb3613bc70195e46eee
Alex Marian <alexandru.marian.ext@infocert.it>
2025-04-07 16:17:16 +0300

    refactoring

---
+++ internal/utils/lines_reader.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func GetLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go readLines(f, lines)
+	return lines
+}
+
+func readLines(f io.ReadCloser, lines chan<- string) {
+	defer f.Close()
+	defer close(lines)
+	currentLineContents := ""
+	for {
+		buffer := make([]byte, 8, 8)
+		n, err := f.Read(buffer)
+		if err != nil {
+			if currentLineContents != "" {
+				lines <- currentLineContents
+				currentLineContents = ""
+			}
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(buffer[:n])
+		parts := strings.Split(str, "\n")
+		for i := 0; i < len(parts)-1; i++ {
+			lines <- fmt.Sprintf("read: %s%s\n", currentLineContents, parts[i])
+			currentLineContents = ""
+		}
+		currentLineContents += parts[len(parts)-1]
+	}
+}
